d0baeff03f5a78f51022fdc9b2b2f102b9d74c86
GicheruBenjamin <projectbuild201@gmail.com>
2024-08-01 10:56:18 +0300

    Ns

--- async.dart
+++ async.dart
@@ -1,5 +1,25 @@
 // Daer also supports async programming 
 
-void main(){
-  
+// Function to fetch data from a server
+Future<String> fetchData() async {
+  // Simulate a delay
+  await Future.delayed(Duration(seconds: 2));
+  // Return some data
+  return 'Data fetched!';
+}
+
+// Call the async function
+void main() {
+  print('Starting...');
+  fetchData().then((value) => print(value));
+  print(eat(3).then((value) => print(value)));
+  print('Continuing...');
+}
+
+
+// Testing
+
+Future <int> eat(int time)async{
+  await Future.delayed(Duration(seconds: time));
+  return time;
 }
