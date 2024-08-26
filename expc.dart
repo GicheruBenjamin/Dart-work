e666187431d0e01405378835cb895f5ab71cb522
GicheruBenjamin <projectbuild201@gmail.com>
2024-08-26 21:09:53 +0300

    C909

---
+++ expc.dart
@@ -0,0 +1,10 @@
+
+
+/*
+ArgumentError: Thrown when one or more arguments provided to a function or method are invalid.
+StateError: Thrown when an application is in an inconsistent state.
+TypeError: Thrown when an operation expects a certain data type but receives a different one.
+FormatException: Thrown when a string does not have the expected format.
+RangeError: Thrown when a value falls outside the acceptable range for a given operation.
+UnsupportedError: Thrown when a requested feature is not supported by the current platform or environment.
+AssertionError: Thrown when an assert statement fails.*/ 
